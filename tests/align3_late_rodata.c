dfa40f74537ad511428e230cc105d96e21c922c2
Anghelo Carvajal <angheloalf95@gmail.com>
2025-08-28 16:47:49 -0400

    Include test

---
+++ tests/align3_late_rodata.c
@@ -0,0 +1,30 @@
+float foo(void) { return 1.1f; }
+#pragma GLOBAL_ASM(
+.section .late_rodata
+
+dlabel var_float
+nonmatching var_float
+.float 1.2
+enddlabel var_float
+
+.align 3
+dlabel var_double
+nonmatching var_double
+.double 1.3
+enddlabel var_double
+
+.section .text
+glabel dummy_func
+nonmatching dummy_func
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+jr $ra
+nop
+endlabel dummy_func
+)
